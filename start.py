f59e67cef32f478cab869a882da2f434cdf8f66a
dionabazi <142183299+dionabazi@users.noreply.github.com>
2024-10-15 10:14:06 +0000

    Delete start.py

--- start.py
+++
@@ -1,9 +0,0 @@
-import pyfiglet
-import subprocess
-
-# Display ASCII art
-ascii_text = pyfiglet.figlet_format("dionabazi")
-print(ascii_text)
-
-# Run your banner script
-subprocess.run(["python3", "banner_generator.py"])
