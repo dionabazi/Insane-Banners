41737e0a5d744c6d659cc9736ff37394cef84a0d
dionabazi <142183299+dionabazi@users.noreply.github.com>
2024-10-15 19:26:50 +0200

    Add files via upload

---
+++ skullmemory.py
@@ -0,0 +1,28 @@
+# Custom ASCII art as a string
+ascii_art = """
+              ___           _,.---,---.,_
+            |         ,;~'             '~;, 
+            |       ,;                     ;,      
+   Frontal  |      ;                         ; ,--- Supraorbital Foramen
+    Bone    |     ,'                         /'
+            |    ,;                        /' ;,
+            |    ; ;      .           . &lt;-'  ; |
+            |__  | ;   ______       ______   ;&lt;----- Coronal Suture
+           ___   |  '/~"     ~" . "~     "~\'  |
+           |     |  ~  ,-~~~^~, | ,~^~~~-,  ~  |
+ Maxilla,  |      |   |        }:{        | &lt;------ Orbit
+Nasal and  |      |   l       / | \       !   |
+Zygomatic  |      .~  (__,.--" .^. "--.,__)  ~. 
+  Bones    |      |    ----;' / | \ `;-&lt;--------- Infraorbital Foramen
+           |__     \__.       \/^\/       .__/  
+              ___   V| \                 / |V &lt;--- Mastoid Process 
+              |      | |T~\___!___!___/~T| |  
+              |      | |`IIII_I_I_I_IIII'| |  
+     Mandible |      |  \,III I I I III,/  | 
+              |       \   `~~~~~~~~~~'    /
+              |         \   .       . &lt;-x---- Mental Foramen
+              |__         \.    ^    ./   
+                            ^~~~^~~~^ 
+"""
+
+print(ascii_art)
---
+++ skullstanding.py
@@ -0,0 +1,126 @@
+# Custom ASCII art as a string
+ascii_art = """
+                                      [/~~~~~~l'
+                                [%};       `rl
+                               Odq            O
+                               B,             8i
+                              vBc            x n
+                             xB xn          xW W
+                             xW wn '     '' xW xn
+                             x$ nxBZB   x`B$x$ %n
+                             xBO.xZJB   xOhB x Z.
+                              x`  hZ "in `n   B
+                              +B[/; '%n$1 ;l "0
+                               `@$  0}.)0 "a$a
+                                xB;(O'''[OU&gt;@n
+                                xWlw0dqqd8*]a,
+                                 +W 000000 w,
+                                  +@'    '0,
+                                    'BB@B'
+                                    OJi"UO.
+                                   "`U``h`i
+                                    B%$%$`i                              
+                          '''''~}))}~W  w0Ma~l'
+                     '~~~C`}0)}~/((OO};])Ok*/z0~~~'''''
+                  ':(8''''OOOOO'! f8B/~~(8fQQfOO/~88888#~''
+                 "8Qf``8qd`#`W#BW'8#`.   80q#BBd,Ot`\``Z t8O
+                wB0c B 0, B@' +0BBBBB   w0BB@W0 v BBW'Bc0B0 xW
+               xW '+,B   wB 0Ww'  +0' +,x00, '0Bn'nB   w0    x
+              xB  B  B  Bn'B'+cB0@BW0   +'BBBBBBB0wBn '@B    x
+              xBi B"U`n %n`ZBh$OU```     ```."OOUB" $O ZBU. xZ
+               hU.`xn hO`h hBBBBBBB$   . BBBBBB$%O.BxBOn Z. xn
+               xJi Bn  B h`OO ```.OO      i""iO"OOUO.x`  xZ xn
+               x!  n  +8c?0(8B$OOa}.      0@%$BBB0]8kW0  xW xn
+               x!  n  "`zX'f8```8QO1     '1?````t'/8pd n xW xn
+               x!  n  _OvM8\0~~~00}`'    `)rOOOO%B*0`&gt;Oc xW xn
+               x!  n  x`;CB#O1['1[  Bi "%O  \t`\t`&gt;'O%0x xW +c
+              xW   n  +B'    +000,wB'000@0W @BBBBBBBB,'n xW  x
+              xW  ',   BwB0WBBW' xB0B@BB@0wW +00000 'w0B xW  x
+              xW  B   x'+0B'BBB wB 'B' 'wB'w  BwW'BBcv B +c  x
+              x$  B    Bi"  ``.O$%ZJB   xBh$B$ `h`Z`."B.  x` x
+              x$  B    BhZ``B."B`.xBB%$$%Bi `hBi"OOBhZ`i  x` x
+              UJ  B    BU`O%`xZ O%BB` ..xBBBOi`$iBBBU.On  x`  n
+              W!  B    BhBB OZJOB};zB[11w8  )@Ovc Q`[%B   x8  n
+              W!  0    0?)0"0$%0.  _B)}}h8    0Ww hOB}`n  x8  n
+              W!   n    c''a BW    zB   x8    'BZO?`&gt;[~.  w8  n
+              W!   c     ``. )0    _B(//k8    0}.`MOa}   wB`  Ji
+             'n xW xn             +cB,  x',              @n   +@'
+             0BWwB'xn               BBBBB                Bn wB  B
+              wBv@Bn     ''B0@W'   vBv,,x0    'B00@'     xB'xB'w0
+             B. n"`    OZ` O   `n   B. "%`  xZ   `$ h$    xBi" x
+            "B.x"U    "B."`     Ji%B`ZhhB`$ixn     `  B    Bi n B
+            xO"UU.    %` J       h"U     `ix.        J`n   Bi n `i
+           xW v,n     W z!      a}  O}))qB  Mi        O,    W!hn c
+           a} n ,     W _!      n  "0    `l v,       [0     @W+M xn
+           * O.x      h'_Q      ;l/] '  l' J,       xZ      xW xO+M
+          x! B x       0%B'    'Oa$i'\. _8 x~l      U.       h xB x
+         xB xBxB        +@'    Bn xW '  ' xB x     B         x  Bn n
+         wBxW0xn          B    0W  +B     B,xB    x'          B,0n xn
+        ', nx B,       ''vB     +@   c  w0'w,    B,B          @W Bn+@
+       "`."JUOn       %`$%O O   Oi hOOUJOB`.    x  O$%`J      x$ hn  B
+       x` xxnB       O.    " $UBZ`$i %ZhnOOOOOO%n "B`. xO      x`xn  Bi
+      x$  xix        BJ`  O"Bn B   x`hnxZBn  x x$OB   O B       B%B   $i
+      +M8 xlv       xB&gt;` xB` W `r~}&gt;OZ;]J'`)}. a}`%~  B 0       `/k  ''w
+      :C8rCU.       +B+  x`  )M'  [a`.   01' 'O,  @'  0a         *kl[0W\
+    [~W'0l'n         0i  +'    `rC}.      `)C0`   %0   n         Ja+q~0cO1
+   O}O)@'wWc          $'  B                       B   ',         ;:C':kB`~
+  w w BnBxWxn         x',  n                      B   B           xc 'nB@'xW
+ wn'B W B wxn         xB,  n                     Bn   0           x0c0vc0 n+'
+'cv'n0W B wxn         +B   c                     Bn  x            xxB nxn +c0n
+O. `OBn B.x.           B$i x                    "B  xn            x h nOJ   `
+    nBxZBZh            Bn  x                    xB  xn            $%BhnBx
+    $BxnBnx             $U  B                   xO. n             nxBxnB%
+    nBhZBJU             @Z~ 0                  xB~ ',             JUBhZBx
+    *Bk*Bv,             x$~  n                 xW  B              +cBk*Bk
+    cBxW0.               h'  Ji                %W  B                0wnBv
+     `]`                 xB! xn               'B} "`                 `;`.
+                          B   x               BB, x
+                          0B  x               BB, x
+                           B'  B             xB   n
+                           hO  `i            x`   n
+                           xO   Ji          xB`  B
+                            B.  xn          xB`  B
+                            B!   +          Br   0
+                            `*    c        [,   x
+                             $1   xn      xB    x
+                            'B}'' +c      ac  ' +O
+                            B x  x +B    B  v00   n
+                            B +' xW B    B  W  n  n
+                           +B  B wxW0    0n xn'n 'n
+                             BZ``JBn      ``%n`JOB
+                           "```BZ`xn     OZ`h$B`h
+                           xOi"   Zh     `OO   %B
+                           Z W\B"Uxn      xZn B w
+                           n W!0v,xn      x*n Bv@
+                           nOW! . xn      x*;C`nx
+                           nB)M'  v,      x*  Ocw
+                           nB x'  n       xB  BxB
+                           c nx'  n       xB  BxB
+                           x n B, n       xB x wB
+                           x $iB. n       xZ x nx
+                            OxnB. n       %Z x nx
+                            BxnB. n       $i x nx
+                            BxWB! n       *  hB w
+                            `UhB! n       *  xBxZ
+                             nxB! n       * "%Bxn
+                             cvB! c       * xB`xn
+                             xnB  xn     'B,xB xn
+                             xnBn xn     B  xn B,
+                             +c0B,+c     B  xn n
+                              x Z. x     Bi xn n
+                              xOZ.  B    Bi n B.
+                               B    B    Bi n B
+                               B     n  x8  MiB
+                               B1'' ',  x8  wnB
+                               `W\`;8   x8~~BnB
+                               O0M1[Bn  xB''h$B
+                               Bc'v, n  x'  BB
+                               B +BBB   x xB  B
+                             v,'cwwn0n  x BW   0c'
+                           UB%ZBhZ J n x$ Z.hZBi"`$
+                           `OJ% Bn x`n  x`nx`h`Zh`U
+                            `.  n   O.   O. h$`hn`
+                                    `    `
+"""
+
+print(ascii_art)
